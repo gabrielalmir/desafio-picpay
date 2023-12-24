bc1ce589b63c4fb29e3ecf6966309dbcfa951a45
gabrielalmir <gabr.almir@gmail.com>
2023-12-24 17:02:55 -0300

    feat: add find customer by document

--- src/main/java/br/com/gabrielalmir/desafiopicpay/repository/customer/CustomerRepository.java
+++ src/main/java/br/com/gabrielalmir/desafiopicpay/repository/customer/CustomerRepository.java
@@ -1,11 +1,12 @@
 package br.com.gabrielalmir.desafiopicpay.repository.customer;
 
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gabrielalmir.desafiopicpay.domain.customer.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-
+    Optional<Customer> findCustomerByDocument(String document);
 }
