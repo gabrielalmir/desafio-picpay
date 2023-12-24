b6068a4e46df2e610915d25e4a44a12ee5ddd3a8
gabrielalmir <gabr.almir@gmail.com>
2023-12-24 10:24:16 -0300

    feat: explicit map to customers table

--- src/main/java/br/com/gabrielalmir/desafiopicpay/domain/customer/Customer.java
+++ src/main/java/br/com/gabrielalmir/desafiopicpay/domain/customer/Customer.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -13,6 +13,7 @@ import java.util.UUID;
 import br.com.gabrielalmir.desafiopicpay.core.contract.Account;
 
 @Entity(name = "customers")
+@Table(name = "customers")
 @Data
 public abstract class Customer implements Account {
     @Id
