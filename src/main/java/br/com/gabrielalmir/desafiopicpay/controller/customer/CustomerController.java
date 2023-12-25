c6b8d5765acdbb3e8ec8f14f0d6f16253eb00f17
gabrielalmir <gabr.almir@gmail.com>
2023-12-25 14:58:38 -0300

    fix: use rest controller instead of spring mvc controller

--- src/main/java/br/com/gabrielalmir/desafiopicpay/controller/customer/CustomerController.java
+++ src/main/java/br/com/gabrielalmir/desafiopicpay/controller/customer/CustomerController.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gabrielalmir.desafiopicpay.domain.entities.customer.Customer;
 import br.com.gabrielalmir.desafiopicpay.presentation.dtos.customer.CreateCustomerDto;
 import br.com.gabrielalmir.desafiopicpay.services.customer.CustomerService;
 
 
-@Controller("/customers")
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
